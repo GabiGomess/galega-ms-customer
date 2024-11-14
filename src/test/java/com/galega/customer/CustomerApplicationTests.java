775ea75b7b8f42529147ff8313a66486d459a39b
Gabriela Gomes <gabrielaofgomes@gmail.com>
2024-11-13 23:51:49 -0300

    atualizar compatibilidade do Java para versão 17 e adicionar teste unitário no CustomerApplicationTests

--- src/test/java/com/galega/customer/CustomerApplicationTests.java
+++ src/test/java/com/galega/customer/CustomerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.galega.customer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class CustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
