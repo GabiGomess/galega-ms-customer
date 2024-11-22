acff90100c6fc9f55d32e13c57d5f4718217b33b
Gabriela Gomes <gabrielaofgomes@gmail.com>
2024-11-21 21:55:13 -0300

    Atualização CustomerApplication

--- src/main/java/com/galega/customer/CustomerApplication.java
+++ src/main/java/com/galega/customer/CustomerApplication.java
@@ -2,8 +2,15 @@ package com.galega.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.galega.customer.infrastructure",
+		"com.galega.customer.adapters",
+		"com.galega.customer.domain"
+})
+
 public class CustomerApplication {
 
 	public static void main(String[] args) {
